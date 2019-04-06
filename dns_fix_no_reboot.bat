11e6f8a19f01122fafbd48bfbad9afcea7f28e3e
Alex N'Guessan <hello@marcaureln.com>
2019-04-06 22:25:10 +0000

    Create dns_fix_no_reboot.bat

---
+++ dns_fix_no_reboot.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+
+ipconfig /flushdns
+ipconfig /registerdns
+ipconfig /release
+ipconfig /renew
+NETSH winsock reset catalog
+NETSH int ipv4 reset reset.log
+NETSH int ipv6 reset reset.log
+
+pause
