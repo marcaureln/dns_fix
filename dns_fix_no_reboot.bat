dd2472a322fd3dc1fe406994eee9611da4f7c76c
Alex N'Guessan <alexmarcaureln@gmail.com>
2019-06-02 13:35:07 +0000

    update the no reboot script

--- dns_fix_no_reboot.bat
+++ dns_fix_no_reboot.bat
@@ -1,12 +1,20 @@
 @echo off
 cls
+title dns_fix
 
-ipconfig /flushdns
-ipconfig /registerdns
-ipconfig /release
-ipconfig /renew
-NETSH winsock reset catalog
-NETSH int ipv4 reset reset.log
-NETSH int ipv6 reset reset.log
+echo Checking for your Windows privilege...
+net session >nul 2>&1
+if %ERRORLEVEL% == 0 (
+    ipconfig /flushdns
+    ipconfig /registerdns
+    ipconfig /release
+    ipconfig /renew
+    NETSH winsock reset catalog
+    NETSH int ipv4 reset reset.log
+    NETSH int ipv6 reset reset.log
+) else (
+    echo You are NOT administrator.
+    echo Exiting...
+)
 
 pause
